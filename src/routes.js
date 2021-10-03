3b8507cda5df5601f13b2cd0a78db7ea1497ae9b
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-03 15:35:06 -0300

    Terminando a primeira aula

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,21 @@ const routes = express.Router();
 
 const views = __dirname + "/views/";
 
+const profile = {
+  name: "Pedro Makengo",
+  avatar: "https://avatars.githubusercontent.com/u/61626420?v=4",
+  "monthly-budget": 3000,
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4,
+};
+
 // Criando as minhas rotas
 routes.get("/", (req, res) => res.render(views + "index"));
 routes.get("/job", (req, res) => res.render(views + "job"));
 routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
-routes.get("/profile", (req, res) => res.render(views + "profile"));
+routes.get("/profile", (req, res) =>
+  res.render(views + "profile", { profile })
+);
 
 module.exports = routes;
