ddd180dd41122e8caaba01b09fc5f8aec4af51c1
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-20 07:19:17 -0300

    Terminando a primeria etapa do projeto

--- src/routes.js
+++ src/routes.js
@@ -79,7 +79,7 @@ const Job = {
           ...job,
           remaining,
           status,
-          budget: Profile.data["value-hour"] * job["total-hours"],
+          budget: Job.services.calculateBudget(job, Profile.data["value-hour"]),
         };
       });
       return res.render(views + "index", { jobs: updatedJobs });
@@ -101,9 +101,34 @@ const Job = {
     show(req, res) {
       const jobId = req.params.id;
 
-      const job = Job.data.find((job) => job.id === jobId);
+      const job = Job.data.find((job) => Number(job.id) === Number(jobId));
+
+      if (!job) {
+        return res.send("Job not found!");
+      }
+
+      job.budget = Job.services.calculateBudget(
+        job,
+        Profile.data["value-hour"]
+      );
 
       return res.render(views + "job-edit", { job });
+    },
+    update(req, res) {
+      const jobId = req.params.id;
+
+      const job = Job.data.find((job) => Number(job.id) === Number(jobId));
+
+      if (!job) {
+        return res.send("Job not found!");
+      }
+
+      const updateJob = {
+        ...job,
+        name: req.body.name,
+        "total-hours": req.body["total-hours"],
+        "daily-hours": req.body["daily-hours"],
+      };
     },
   },
 
@@ -124,6 +149,7 @@ const Job = {
       // restam x dias
       return dayDiff;
     },
+    calculateBudget: (job, valueHour) => valueHour * job["total-hours"],
   },
 };
 
@@ -132,6 +158,7 @@ routes.get("/", Job.controllers.index);
 routes.get("/job", Job.controllers.create);
 routes.post("/job", Job.controllers.save);
 routes.get("/job/:id", Job.controllers.show);
+routes.post("/job/:id", Job.controllers.update);
 routes.get("/profile", Profile.controllers.index);
 routes.post("/profile", Profile.controllers.update);
 
